bfae04ef619bb7e18b8f1df7deb5ee47894c46b7
Luciano Ferreira <lucianoneto1033@gmail.com>
2023-06-06 14:29:43 -0300

    first commit

---
+++ src/main/java/com/hackaton/emergenciaEscolar/EmergenciaEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.emergenciaEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergenciaEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergenciaEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/emergenciaEscolar/EmergenciaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.emergenciaEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergenciaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
